3a5204d766134878502d61fcf52f914d1be1c5d1
KhaledAhmed2004 <khaledahmednayeem2004.com>
2023-11-07 02:39:51 +0600

    add some minor update

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import iceCream from "../../assets/iceCream.jpg";
 import { MdTableBar } from "react-icons/md";
 import { BiFoodMenu } from "react-icons/bi";
 
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row h-[580px]  md:p-5 my-8 gap-5 lg:gap-0">
-      <div className="md:w-[55%] lg:w-[60%] space-y-3 px-6 lg:px-8 flex flex-col justify-center">
+      <div className="md:w-[55%] lg:w-[60%] space-y-3 md:space-y-7 px-6 lg:px-8 flex flex-col justify-center">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold text-center md:text-left">
           We are always here to serve you.
         </h1>
@@ -28,8 +29,11 @@ const Banner = () => {
       </div>
       <div className="grid h-full md:w-[40%] grid-cols-12 grid-rows-6 gap-4 px-6 md:px-0">
         <div className="col-span-6 row-span-3 rounded-xl bg-[url('https://i.ibb.co/G0MBkfW/chad-montano-Mq-T0asuo-Ic-U-unsplash.jpg')] bg-cover bg-center bg-no-repeat hover:translate-y-[-15px] duration-200 hover:shadow-xl"></div>
+
         <div className="col-start-7 col-end-12 row-start-2 row-end-4 rounded-xl bg-[url('https://i.ibb.co/gt5mBHh/courtney-cook-xt-Vj-WVCi-E-U-unsplash.jpg')] bg-cover bg-center bg-no-repeat hover:translate-y-[-10px] duration-200 hover:shadow-xl"></div>
+
         <div className="col-span-5 row-span-3 rounded-xl bg-[url('https://i.ibb.co/T0FW3HC/monika-grabkowska-y6-A9bh-ILk-M-unsplash.jpg')] bg-cover bg-center bg-no-repeat hover:translate-y-[-10px] duration-200 hover:shadow-xl"></div>
+
         <div className="col-span-7 row-span-3 rounded-xl bg-[url('https://i.ibb.co/1sC1g7M/shenggeng-lin-Xo-N3v3-Ge7-EE-unsplash.jpg')] bg-cover bg-center bg-no-repeat hover:translate-y-[-10px] duration-200 hover:shadow-xl"></div>
       </div>
     </div>
